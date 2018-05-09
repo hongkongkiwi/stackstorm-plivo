27fbb4e05abc670c1aac6652bbc2a88928619dbb
Andy <andy@savage.hk>
2018-05-09 23:47:50 +0800

    Updated to support Plivo

--- actions/make_call.py
+++ actions/make_call.py
@@ -2,19 +2,19 @@ import plivo
 
 from st2common.runners.base_action import Action
 
-class PlivoSendSMSAction(Action):
+class PlivoMakeCallAction(Action):
     def __init__(self, config):
-        super(PlivoSendSMSAction, self).__init__(config=config)
+        super(PlivoMakeCallAction, self).__init__(config=config)
         self.client = plivo.RestClient(auth_id=self.config['auth_id'],
                                        auth_token=self.config['auth_token'])
 
-    def run(self, from_number, to_number, text):
+    def run(self, from_number, to_number, answer_url):
         try:
-            self.client.messages.create(text=text, src=from_number, dst=to_number)
+            self.client.calls.create(answer_url=answer_url, from_=from_number, to_=to_number)
         except Exception as e:
-            error_msg = ('Failed sending sms to: %s, exception: %s\n' %
+            error_msg = ('Failed making a call to: %s, exception: %s\n' %
                          (to_number, str(e.msg)))
             self.logger.error(error_msg)
             raise Exception(error_msg)
 
-        self.logger.info('Successfully sent sms to: %s\n' % (to_number))
+        self.logger.info('Successfully make call to: %s\n' % (to_number))
